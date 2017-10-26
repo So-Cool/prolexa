f83140ed2829aaf328b39c0c97b8131515c337f3
Peter Flach <Peter.Flach@bristol.ac.uk>
2017-10-26 11:28:00 +0100

    added George and Peter

--- alexa_mod.pl
+++ alexa_mod.pl
@@ -310,6 +310,8 @@ determiner(s,X=>B,X=>H,[(H:-B)]) --> [every].
 determiner(p, sk=>H1, sk=>H2, [(H1:-true),(H2 :- true)]) -->[some].
 
 proper_noun(s,sam) --> [sam].
+proper_noun(s,george) --> [george].
+proper_noun(s,peter) --> [peter].
 noun(s,X=>human(X)) --> [human].
 noun(p,X=>human(X)) --> [humans].
 noun(s,X=>living_being(X)) --> [living],[being].
